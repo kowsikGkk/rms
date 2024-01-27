aa5dab95d34b8d00bc0cad36a4fd8fb2452cedd0
kowsikc2c <kowsikc2c@gmail.com>
2024-01-27 23:40:57 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import Login from "./Pages/Login/Login";
+import Register from "./Pages/Register/Register";
+import Home from "./Pages/Home/Home";
+import Menu from "./Pages/Menu/Menu";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+     {localStorage.getItem('accessToken') ? <Routes>
+          {/* <Route index element={<Dashboard />} /> */}
+          <Route path="/dashboard" element={<Dashboard />}/>
+      </Routes>:
+      <Routes>
+          <Route path="/" element={<Login />}/>
+          <Route path="/register" element={<Register />}/>
+          <Route path="*" element={<div>no pre login page</div>}/>
+        </Routes>}
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
---
+++ src/Pages/Menu/Menu.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Menu(props) {
+    return (
+        <div>
+            menu
+        </div>
+    );
+}
+
+export default Menu;
